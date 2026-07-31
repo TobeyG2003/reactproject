bb69c08efa252edab03da8be604341ccfa7f7505
Tobey Gray <TobeyG2003@gmail.com>
2026-07-30 21:15:27 -0400

    updates navbar layout and adds dropdown and navigation to new wip pages

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,7 +5,9 @@ import { Page2 } from './pages/page2'
 import { Search } from './pages/search'
 import { Login } from './pages/login'
 import { Signup } from './pages/signup'
+import { ProfileSettings } from './pages/profileSettings'
 import { Layout } from './layout'
+import { Profile } from './pages/profile'
 import { AuthProvider } from './AuthContext'
 
 function App() {
@@ -19,6 +21,8 @@ function App() {
           <Route path="/search" element={<Search/>} />
           <Route path="/login" element={<Login/>} />
           <Route path="/signup" element={<Signup/>} />
+          <Route path="/profilesettings" element={<ProfileSettings/>} />
+          <Route path="/profile" element={<Profile/>} />
         </Route>
       </Routes>
     </Router>
--- my-react-app/src/components/navbar.jsx
+++ my-react-app/src/components/navbar.jsx
@@ -1,15 +1,33 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import '../App.css'
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { AuthContext } from '../AuthContext'
 import { useContext } from 'react';
 import { CgProfile } from "react-icons/cg";
+import { MdArrowDropDown } from "react-icons/md";
 
 export function Navbar() {
+  const navigate = useNavigate();
+  
   const { userdata, logoutUser } = useContext(AuthContext);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => setIsOpen((prev) => !prev);
+  const dropdownRef = useRef(null);
 
   const location = useLocation(); 
   const currentPath = location.pathname;
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
 
   return (
     <>
@@ -17,7 +35,7 @@ export function Navbar() {
       width: '100%',
       boxSizing: 'border-box',
       padding: '10px',
-      height: '40px',
+      height: '45px',
       display: 'flex', 
       flexDirection: 'row', 
       gap: '16px',
@@ -30,24 +48,63 @@ export function Navbar() {
       <div
       style={{ marginRight: 'auto', display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px' }}
     >
-      <p>React Forum</p>
+      <p style={{color: '#ffffff'}}>React Forum</p>
     </div>
-      <Link to="/" className={currentPath === '/' ? "navbarLinkSelected" : "navbarLink"}
-        onClick={() => setOpenPage('page1')}
-      >
+      <Link to="/" className={currentPath === '/' ? "navbarLinkSelected" : "navbarLink"}>
         Go to Page 1
       </Link>
-      <Link to="/page2" className={currentPath === '/page2' ? "navbarLinkSelected" : "navbarLink"}
-        onClick={() => setOpenPage('page2')}
-      >
+      <Link to="/page2" className={currentPath === '/page2' ? "navbarLinkSelected" : "navbarLink"}>
         Go to Page 2
       </Link>
-      <Link to="/search" className={currentPath === '/search' ? "navbarLinkSelected" : "navbarLink"}
-        onClick={() => setOpenPage('search')}
-      >
+      <Link to="/search" className={currentPath === '/search' ? "navbarLinkSelected" : "navbarLink"}>
         Search
       </Link>
-      <Link to="/login" className={currentPath === '/login' ? "navbarLinkSelected" : "navbarLink"}
+      {userdata ? (
+        <div
+          style={{ marginLeft: 'auto', display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px' }}
+          >
+            <div className="dropdown-container" ref={dropdownRef} style={{ position: "relative", display: "inline-block", height: '30px' }}>
+      <MdArrowDropDown size = {30} style = {{color: isOpen? '#ffffff':''}} className="dropdown-button" onClick={toggleDropdown} />
+      {isOpen && (
+        <ul className="dropdown-menu" style={{ position: "absolute", listStyle: "none", margin: 0, padding: "5px 0", border: "1px solid #ccc", zIndex: 100 }}>
+            <li 
+              onClick={() => handleOptionClick(option)}
+              style={{ padding: "8px 16px", }}
+            >
+              <Link to="/profilesettings" className={currentPath === '/profilesettings' ? "navbarLinkSelected" : "navbarLink"}
+        onClick={() => setOpenPage('search')}
+      >
+        Profile Settings
+      </Link>
+            </li>
+            <li 
+              onClick={() => handleOptionClick(option)}
+              style={{ padding: "8px 16px",}}
+            >
+              <Link className='navbarLink' onClick={logoutUser}>
+        Logout
+      </Link>
+            </li>
+        </ul>
+      )}
+    </div>
+          <p style={{color: '#ffffff'}}>Welcome, {userdata.display_name}!</p>
+          {userdata.profile_picture_url ? 
+            (<img class = "pfp"
+              src={'data:image/png;base64,'+userdata.profile_picture_url} 
+              alt="Profile" 
+              style={{ width: '35px', height: '35px', borderRadius: '50%' }} 
+            />
+            ): (
+              <div className='pfp' onClick={() => navigate('/profile')}><CgProfile
+              style={{color: '#ffffff', width: '35px', height: '35px', borderRadius: '50%' }}  />
+              </div>
+            )}
+        </div>
+      ) : (
+        <div
+        style={{ marginLeft: 'auto', display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px' }}>
+          <Link to="/login" className={currentPath === '/login' ? "navbarLinkSelected" : "navbarLink"}
         onClick={() => setOpenPage('login')}
       >
         Login
@@ -57,29 +114,6 @@ export function Navbar() {
       >
         Signup
       </Link>
-      <Link className='navbarLink' onClick={logoutUser}>
-        Logout
-      </Link>
-      {userdata ? (
-        <div
-          style={{ marginLeft: 'auto', display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px' }}
-          >
-          <p>welcome {userdata.display_name}</p>
-          {userdata.profile_picture_url ? 
-            (<img 
-              src={'data:image/png;base64,'+userdata.profile_picture_url} 
-              alt="Profile" 
-              style={{ width: '40px', height: '40px', borderRadius: '50%' }} 
-            />
-            ): (
-              <CgProfile
-              style={{ width: '40px', height: '40px', borderRadius: '50%' }}  />
-            )}
-        </div>
-      ) : (
-        <div
-        style={{ marginLeft: 'auto', display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px' }}>
-          <p>no user</p>
         </div>
       )}
       </div>
